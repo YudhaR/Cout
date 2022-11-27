25370ddf8b79b535130e0381d9963d7a409d580a
YudhaR <yudharizqia182@gmail.com>
2022-11-28 04:31:09 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/*
+    Program Cout
+    Ket : Program Cout
+    By  : Yudha Rizqia Grafer
+    Tgl : 14 September 2022
+
+*/
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "I'm Learning C++";
+    return 0;
+}
